2d390196bfa8eceaa4e524a72acc161481c79f1b
Nagare Tushar <145959296+Tusshar123@users.noreply.github.com>
2025-08-09 22:51:11 +0530

    Add files via upload

---
+++ Loop's_2.java/Do.java
@@ -0,0 +1,10 @@
+public class Do {
+    public static void main(String[] args) {
+        int i =12;
+        while(i<11){
+            System.out.println(" hallo" +i);
+            i=i+1;
+        }
+    }
+    
+}
