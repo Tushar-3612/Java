7a4f57231c57f7f1a3a688088a5b9268eae333ce
Nagare Tushar <145959296+NAGARETUSHAR@users.noreply.github.com>
2025-08-16 05:38:17 +0530

    Hollow butterfly.java

---
+++ Patterns.java/Hollow butterfly.java
@@ -0,0 +1,60 @@
+public class HollowButterfly {
+    public static void main(String[] args) {
+        int n = 5; // you can change this value to make it bigger or smaller
+
+        // Top half
+        for (int i = 1; i <= n; i++) {
+            // Left wing
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || j == i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            // Spaces in middle
+            for (int s = 1; s <= 2 * (n - i); s++) {
+                System.out.print(" ");
+            }
+
+            // Right wing
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || j == i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        // Bottom half
+        for (int i = n; i >= 1; i--) {
+            // Left wing
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || j == i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            // Spaces in middle
+          int spaces=2*(n-i);
+            for (int s = 1; s <=spaces; s++) {
+                System.out.print(" ");
+            }
+
+            // Right wing
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || j == i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+  }
