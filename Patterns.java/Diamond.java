eaf1edf40a960b339a44a82a301d223c8cf44050
Nagare Tushar <145959296+Tushar-3612@users.noreply.github.com>
2025-08-17 21:08:19 +0530

    Diamond.java

---
+++ Patterns.java/Diamond.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Solutions (
+
+public static void main(String args[]) {
+
+int n = 57
+
+//upper part
+
+for(int i = 1 1 <= n i++) {
+
+//spaces
+
+for(int j = 1 j <= n - 1 j++) {
+
+}
+
+System.out.print(" ");
+
+for (int j = 1 j<=2*1-1; j++) (
+
+}
+
+System.out.print("=");
+
+System.out.println();
+
+//lower part
+
+for(int i=n; i >= 1 i--) {
+
+//spaces
+
+for (int f = 1 j <= n - 1 j++) {
+
+}
+
+System.out.print(" ");
+
+for (int j = 1 j<=2*1-1; j++) {
+
+System.out.print("*");
+
+System.out.println();
