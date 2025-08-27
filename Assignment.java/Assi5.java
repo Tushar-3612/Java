eaed3e10dca88e6c7e195a3298c67b9ac0952298
Nagare Tushar <145959296+Tushar-3612@users.noreply.github.com>
2025-08-27 10:21:20 +0530

    Assi5.java

--- Assignment.java/Assi5.java
+++ Assignment.java/Assi5.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class Assi5 {
-    
+    public static void main(String args[]){
+
+        Scanner sc =new Scanner(System.in);
+         System.out.print("Enter You are Age ");
+        int age = sc.nextInt();
+       
+        if(age>=18){
+            System.out.println("You Are Eligible For Voting ");
+        }else{
+            System.out.println("You are not eligible ");
+        }
+
+    }
 }
