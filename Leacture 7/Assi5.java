7db0ccd4fc5cb27bd32c3f35437d46ab0fd29304
Nagare Tushar <145959296+Tushar-3612@users.noreply.github.com>
2025-09-13 00:32:20 +0530

    Add files via upload

---
+++ Leacture 7/Assi5.java
@@ -0,0 +1,21 @@
+public class Assi5 {
+    public static void reverse(int num[]){
+        int start=0 , end=num.length-1;
+        while(start<end){
+            int temp = num[end];
+            num[end]=num[start];
+            num[start]=temp;
+            start++;
+            end--;
+        }
+    }
+    public static void main(String[] args) {
+        int num[]={1,2,4,3,5};
+        reverse(num);
+        for(int i=0;i<num.length;i++){
+            System.out.print(num[i]+" ");
+
+        }
+        System.out.println();
+    }
+}
---
+++ Leacture 7/Assi6.java
@@ -0,0 +1,18 @@
+public class Assi6 {
+    public static void pairs(int num[]){
+        int count =0;
+        for(int i=0;i<num.length;i++){
+            int curr=num[i];
+            for(int j=i+1;j<num.length;j++){
+                System.out.print("("+curr+","+num[j]+") ");
+                count++;
+            }
+            System.out.println();
+        }
+        System.out.println("Total pair "+count);
+    }
+    public static void main(String[] args) {
+        int num[]={2,4,6,8,10};
+        pairs(num);
+    }
+}
